a224995ef612487bdf6e21dcf92da749dbef8c42
venky0918 <venkyvidala@gmail.com>
2025-08-09 14:20:26 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="venkatesh",
+    author_email="venkyvidala@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
